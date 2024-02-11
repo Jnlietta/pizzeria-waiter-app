01d7feb3016da8f4a1f52e82a5f4407ab2561d0f
Julia Glinka <julia.w.glinka@gmail.com>
2024-02-11 20:10:51 +0100

    Add package clsx and change code in NavBar

--- src/components/views/NavBar/NavBar.js
+++ src/components/views/NavBar/NavBar.js
@@ -1,7 +1,21 @@
-const NavBar = () => {
-    return (
-      <h1>NavBar</h1>
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from 'react-router-dom';
+import clsx from 'clsx';
+
+
+const NavBar = props => {
+    return(
+        <Navbar bg="primary" data-bs-theme="dark" expand="lg" className={clsx('mb-4','rounded', 'navbar-dark')}>
+            <Container>
+                <Navbar.Brand href="#home">Waiter.app</Navbar.Brand>
+                <Nav className="justify-content-end">
+                    <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
     );
-  };
-  
-  export default NavBar;
+};
+
+export default NavBar;
