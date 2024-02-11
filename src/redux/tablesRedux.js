94a22d536cc51a8622e27ea64999e382f3d56331
Julia Glinka <julia.w.glinka@gmail.com>
2024-02-11 23:08:15 +0100

    Add action UBDATE_TABLES

--- src/redux/tablesRedux.js
+++ src/redux/tablesRedux.js
@@ -2,10 +2,15 @@
 
 // actions
 const createActionName = actionName => `app/tables/${actionName}`;
+const UBDATE_TABLES = createActionName('UBDATE_TABLES');
 
 // action creators
+export const ubdateTables = payload => ({ type: UBDATE_TABLES, payload });
+
 const tablesReducer = (statePart = [], action) => {
   switch (action.type) {
+    case UBDATE_TABLES:
+      return [...action.payload];
     default:
       return statePart;
   };
