3dbd3b8eb2d4dad1be3197981ac81a062e33ced0
Julia Glinka <julia.w.glinka@gmail.com>
2024-02-11 20:00:59 +0100

    Import Header and Footer to App

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import { Container } from 'react-bootstrap';
 import Home from './components/pages/Home/Home.js';
 import Table from './components/pages/Table/Table.js';
 import NotFound from './components/pages/NotFound/NotFound.js';
+import Header from './components/views/Header/Header.js';
+import Footer from './components/views/Footer/Footer.js';
 
 
 const App = () => {
   return ( 
-    <main>
-      <h1>Hello world</h1>
       <Container>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/table/:tableId" element={<Table />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
+        <Footer />
       </Container>
-    </main>
   );
 };
 
