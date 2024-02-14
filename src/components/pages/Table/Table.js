e5e798123812c2ecc3eb95bebef90eb626852bda
Julia Glinka <julia.w.glinka@gmail.com>
2024-02-14 16:15:25 +0100

    Use selector getTableById in Table

--- src/components/pages/Table/Table.js
+++ src/components/pages/Table/Table.js
@@ -1,13 +1,19 @@
 import { useParams } from "react-router-dom";
 import TableForm from "../../features/TableForm/TableForm";
+import { useSelector } from "react-redux";
+import { getTableById } from "../../../redux/tablesRedux";
 
 const Table = () => {
   const {tableId} = useParams();
+  const tableToEdit = useSelector(state => getTableById(state, tableId));
 
     return (
       <div>
         <h1>Table {tableId}</h1>
-        <TableForm tableId={tableId}/>
+        <TableForm 
+          tableId={tableId}
+          {...tableToEdit}
+        />
       </div>
 
     );
