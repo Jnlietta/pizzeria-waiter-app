3e8b22674c57aa53dd6b5bdb517bc49932d28205
Julia Glinka <julia.w.glinka@gmail.com>
2024-02-14 16:01:41 +0100

    Add layout for TableForm and style it

--- src/components/features/TableForm/TableForm.js
+++ src/components/features/TableForm/TableForm.js
@@ -1,7 +1,52 @@
-const TableForm = () => {
+import { Button, Form } from "react-bootstrap";
+import styles from "./TableForm.module.scss";
+
+const TableForm = ({ tableId }) => {
 
     return(
-        <h1>PostForm</h1>
+        <Form>
+            <div className={styles.line}>
+                <strong>Status:</strong>
+                <Form.Select 
+                    style={{ width: '220px', marginLeft: '15px'}}
+                    aria-label="Default select example"
+                    // value=
+                    // onChange= 
+                    >
+                    <option>Select status...</option>
+                </Form.Select>
+            </div>
+            <div className={styles.line}>
+                <strong>People:</strong>
+                <Form.Control
+                    className={styles.input}
+                    style={{ width: '50px' }}
+                    value="2"
+                    // onChange={e => setTitle(e.target.value)}
+                    type="text" placeholder="Enter title"
+                    />
+                <span>/</span>
+                <Form.Control
+                    className={styles.input}
+                    style={{ width: '50px' }}
+                    value="2"
+                    // onChange={e => setTitle(e.target.value)}
+                    type="text" placeholder="Enter title"
+                    />
+            </div>
+            <div className={styles.line}>
+                <strong>Bill:</strong>
+                <span className={styles.bill}>$</span>
+                <Form.Control
+                    className={styles.input}
+                    style={{ width: '50px'}}
+                    value="345"
+                    // onChange={e => setTitle(e.target.value)}
+                    type="text" placeholder="Enter title"
+                    />
+            </div>
+            <Button variant="primary" type="submit">Update</Button>
+        </Form>
     );
 };
 
